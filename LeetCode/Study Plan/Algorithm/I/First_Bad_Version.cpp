4900d4660c57b1c36b79a5e8975bbe839f1908fb
Ashutosh Deshpande <36955661+DaemonStark@users.noreply.github.com>
2022-01-24 23:45:58 +0530

    First Bad Version Solution Added

---
+++ LeetCode/Study Plan/Algorithm/I/First_Bad_Version.cpp
@@ -0,0 +1,19 @@
+//LeetCode - First Bad Version Solution
+
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
